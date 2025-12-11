a24362ec61e53fc2c19d95f32b4b85f1675bf94b
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-11 23:01:34 +0300

    implement product type entity

---
+++ internal/domain/product_type.go
@@ -0,0 +1,22 @@
+package domain
+
+import "time"
+
+// ProductType описывает тип продукта
+type ProductType struct {
+	ID         int64
+	Name       string
+	Price      int64 // Цена хранится в копейках
+	CategoryID int64
+	CreatedAt  time.Time
+	UpdatedAt  *time.Time
+	IsActive   bool
+}
+
+func NewProductType(name string, price int64, categoryID int64) *ProductType {
+	return &ProductType{
+		Name:       name,
+		Price:      price,
+		CategoryID: categoryID,
+	}
+}
