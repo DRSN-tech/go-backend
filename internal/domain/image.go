2715718d77f171f51440e9b879585a88e50db66b
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-17 01:36:02 +0300

    add Bytes and MimeType to Image struct

--- internal/domain/image.go
+++ internal/domain/image.go
@@ -5,18 +5,20 @@ type Image struct {
 	ID        string // uuid
 	Bucket    string
 	ObjectKey string
+	Bytes     []byte
 	// Передайте значение -1 в Size, если размер потока неизвестен
 	// (внимание: при передаче значения -1 будет выделен большой объем памяти).
-	Size        *int64
-	ContentType *string // Example: "application/text"
+	Size     *int64
+	MimeType *string // Example: "application/text"
 }
 
-func NewImage(id string, bucket string, objectKey string, size *int64, contentType *string) *Image {
+func NewImage(id string, bucket string, objectKey string, bytes []byte, size *int64, mimeType *string) *Image {
 	return &Image{
-		ID:          id,
-		Bucket:      bucket,
-		ObjectKey:   objectKey,
-		Size:        size,
-		ContentType: contentType,
+		ID:        id,
+		Bucket:    bucket,
+		ObjectKey: objectKey,
+		Bytes:     bytes,
+		Size:      size,
+		MimeType:  mimeType,
 	}
 }
