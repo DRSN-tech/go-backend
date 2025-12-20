3b7b7c3b90107e322247c2dce83dd52bc57aae24
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-20 05:20:15 +0300

    add Infra struct

--- internal/usecase/infrastructure.go
+++ internal/usecase/infrastructure.go
@@ -10,3 +10,15 @@ type ImagesInfra interface {
 	UploadImages(ctx context.Context, req *UploadImagesReq) (*UploadImagesRes, error)
 	CleanupImages(keys []string)
 }
+
+type Infrastructure struct {
+	MlServiceInfra MlServiceInfra
+	ImagesInfra    ImagesInfra
+}
+
+func NewInfrastructure(mlServiceInfra MlServiceInfra, imagesInfra ImagesInfra) *Infrastructure {
+	return &Infrastructure{
+		MlServiceInfra: mlServiceInfra,
+		ImagesInfra:    imagesInfra,
+	}
+}
