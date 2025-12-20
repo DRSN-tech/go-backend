835597619bbec2eed7bca75d04d02062d9e95d44
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-20 05:16:36 +0300

    change converter

--- internal/repository/pgdb/converter/converter.go
+++ internal/repository/pgdb/converter/converter.go
@@ -1,18 +1,34 @@
-//go:generate goverter gen go-backend/internal/repository/pgdb/converter
+//go:generate goverter gen github.com/DRSN-tech/go-backend/internal/repository/pgdb/converter
 package converter
 
 import (
+	"time"
+
 	"github.com/DRSN-tech/go-backend/internal/domain"
 )
 
 // ProductConverter преобразует сущности Product между domain и моделью PostgreSQL.
+// goverter:converter
+// goverter:extend ConvertTime
+// goverter:extend ConvertPointerTime
 type ProductConverter interface {
 	ToModel(entity *domain.Product) *ProductModel
 	ToEntity(model *ProductModel) *domain.Product
 }
 
 // CategoryConverter преобразует сущности Category между domain и моделью PostgreSQL.
+// goverter:converter
+// goverter:extend ConvertTime
+// goverter:extend ConvertPointerTime
 type CategoryConverter interface {
 	ToModel(entity *domain.Category) *CategoryModel
 	ToEntity(model *CategoryModel) *domain.Category
+}
+
+func ConvertPointerTime(t *time.Time) *time.Time {
+	return t
+}
+
+func ConvertTime(t time.Time) time.Time {
+	return t
 }
