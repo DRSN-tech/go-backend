1e9ebc7339fa3ecbf43106b75db3f7066a122bfd
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-20 23:03:06 +0300

    add error handling

--- internal/delivery/v1/grpc/helpers.go
+++ internal/delivery/v1/grpc/helpers.go
@@ -1,6 +1,8 @@
 package grpc
 
 import (
+	"errors"
+
 	"github.com/DRSN-tech/go-backend/pkg/e"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -8,6 +10,8 @@ import (
 
 func GRPCErrorResponse(err error) error {
 	switch {
+	case errors.Is(err, e.ErrNoProducts):
+		return status.Error(codes.NotFound, err.Error())
 	default:
 		return status.Error(codes.Internal, e.ErrInternalServerError.Error())
 	}
