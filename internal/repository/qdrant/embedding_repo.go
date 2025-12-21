5ac5221714526d5af22e142832ef61e5e4fedea8
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-22 02:58:00 +0300

    add del method

--- internal/repository/qdrant/embedding_repo.go
+++ internal/repository/qdrant/embedding_repo.go
@@ -23,7 +23,7 @@ func NewEmbeddingRepo(client *qdrant.Client, cfg *cfg.QdrantCfg) *EmbeddingRepo 
 	}
 }
 
-// Upsert сохраняет или обновляет embedding-векторы в указанной коллекции Qdrant.
+// Upsert сохраняет или обновляет embedding-векторы в указанной коллекции.
 func (q *EmbeddingRepo) Upsert(ctx context.Context, vectors []domain.Embedding) ([]domain.Embedding, error) {
 	reqVectors := make([]*qdrant.PointStruct, 0, len(vectors))
 	for _, vector := range vectors {
@@ -43,4 +43,27 @@ func (q *EmbeddingRepo) Upsert(ctx context.Context, vectors []domain.Embedding) 
 	}
 
 	return vectors, nil
+}
+
+// Delete удаляет векторы по их ID в указанной коллекции.
+func (q *EmbeddingRepo) Delete(ctx context.Context, vectors []domain.Embedding) error {
+	ids := make([]*qdrant.PointId, 0, len(vectors))
+	for _, vector := range vectors {
+		ids = append(ids, qdrant.NewIDUUID(vector.ID))
+	}
+
+	pointsSelector := &qdrant.PointsSelector{
+		PointsSelectorOneOf: &qdrant.PointsSelector_Points{Points: &qdrant.PointsIdsList{
+			Ids: ids,
+		}},
+	}
+
+	if _, err := q.client.Delete(ctx, &qdrant.DeletePoints{
+		CollectionName: q.cfg.QdrantCollectionName,
+		Points:         pointsSelector,
+	}); err != nil {
+		return e.Wrap(whereami.WhereAmI(), err)
+	}
+
+	return nil
 }
