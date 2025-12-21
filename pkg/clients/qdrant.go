3ca63191571e7b9b8c9314421b49e46939bc4df1
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-21 19:43:44 +0300

    change EnsureCollection method

--- pkg/clients/qdrant.go
+++ pkg/clients/qdrant.go
@@ -32,17 +32,17 @@ func NewQdrantClient(cfg *config.QdrantCfg) (*QdrantClient, error) {
 	}, nil
 }
 
-func EnsureCollection(ctx context.Context, client *QdrantClient) error {
-	exists, err := client.Client.CollectionExists(ctx, client.cfg.QdrantCollectionName)
+func (q *QdrantClient) EnsureCollection(ctx context.Context) error {
+	exists, err := q.Client.CollectionExists(ctx, q.cfg.QdrantCollectionName)
 	if err != nil {
 		return fmt.Errorf("failed to check collection existence: %w", err)
 	}
 
 	if !exists {
-		if err := client.Client.CreateCollection(ctx, &qdrant.CreateCollection{
-			CollectionName: client.cfg.QdrantCollectionName,
+		if err := q.Client.CreateCollection(ctx, &qdrant.CreateCollection{
+			CollectionName: q.cfg.QdrantCollectionName,
 			VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
-				Size:     client.cfg.VectorSize,
+				Size:     q.cfg.VectorSize,
 				Distance: qdrant.Distance_Cosine,
 			}),
 		}); err != nil {
