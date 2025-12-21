1c20dedc0a0a05937d1f04c37905757834231bb8
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-12-22 02:57:33 +0300

    change method

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -3,6 +3,6 @@ package usecase
 import "context"
 
 type ProductUC interface {
-	RegisterNewProduct(ctx context.Context, req *AddNewProductReq) error
+	RegisterNewProduct(ctx context.Context, req *AddNewProductReq) (*OutboxEvent, error)
 	GetProductsInfo(ctx context.Context, req *GetProductsReq) (*GetProductsRes, error)
 }
