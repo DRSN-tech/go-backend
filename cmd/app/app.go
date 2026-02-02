998b5513d6155ef1acd139fb12e2bf602fc923be
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2026-02-02 17:30:15 +0300

    refactor: encapsulate application initialization, dependencies, and lifecycle management within a new `App` struct.

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/DRSN-tech/go-backend/internal/app"
+import (
+	"os"
+
+	"github.com/DRSN-tech/go-backend/internal/app"
+	config "github.com/DRSN-tech/go-backend/internal/cfg"
+	"github.com/DRSN-tech/go-backend/pkg/logger"
+)
 
 func main() {
-	app.Run()
+	log := logger.NewSlogLogger()
+
+	cfg, err := config.Load(log)
+	if err != nil {
+		log.Errorf(err, "failed to load config")
+		os.Exit(1)
+	}
+
+	application, err := app.NewApp(cfg, log)
+	if err != nil {
+		log.Errorf(err, "failed to initialize app")
+		os.Exit(1)
+	}
+
+	if err := application.Run(); err != nil {
+		os.Exit(1)
+	}
 }
